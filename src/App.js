e8ce920f938e476ce80e2dea4f4080818e3bd508
PRATHAMESHG1234 <ghorpadeprathamesh2411@gmail.com>
2023-08-28 21:34:41 +0530

    complet code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css"
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import { useSelector,useDispatch } from "react-redux";
+import { incNumber,DecNumber } from "./Actions/index";
 function App() {
+  const myState = useSelector((state) =>
+     state.changeNumber
+  )
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-center">
+      <h1>Increment And Decrement Counter</h1>
+      <h4>Using React And Redux</h4>
+      <div className="quantity ">
+        <a href="#" name="quqntity_minus" title="Decrement" onClick={() => dispatch(DecNumber(6))}><span> <RemoveIcon /></span></a>
+        <input id="myInput" name="quantity" type="text" className="quantity_input" value={myState} />
+        <a href="#" name="quqntity_plus" title="Increment" onClick={() => dispatch(incNumber(5))}><span> <AddIcon /> </span></a>
+      </div>
     </div>
   );
 }
---
+++ src/Reducers/index.js
@@ -0,0 +1,10 @@
+import changeNumber from "./PlusMinus";
+
+
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+    changeNumber:changeNumber
+})
+export default rootReducer
+
